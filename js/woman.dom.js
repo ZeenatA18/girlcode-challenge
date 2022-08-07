e0a3c5be34909545d8b50e1f11d86500c5684c18
SipheMkhosi <siphemkhosi7@gmail.com>
2022-08-07 06:19:56 +0200

    added new and modified files

--- js/woman.dom.js
+++ js/woman.dom.js
@@ -1,22 +1,154 @@
+
+
 const mediaSelector = document.getElementById("media");
-// const  mediaSelector = document.querySelector(
-//     'input[name="radioButn"]:checked'
-//   ).value;
+
+const webCamContainer =
+	document.getElementById("web-cam-container");
 
 let selectedMedia = null;
 
+
+let chunks = [];
+
+
 mediaSelector.addEventListener("change", (e) => {
+
 	selectedMedia = e.target.value;
+
 	document.getElementById(
-	`${selectedMedia}-recorder`).style.display = "block";
+		`${selectedMedia}-recorder`)
+			.style.display = "block";
+
 	document.getElementById(
-	`${otherRecorder(selectedMedia)}-recorder`)
-	.style.display = "none";
+			`${otherRecorderContainer(
+			selectedMedia)}-recorder`)
+		.style.display = "none";
 });
 
-function otherRecorder(selectedMedia) {
-	return selectedMedia === "vidtype" ? "audtype" : "vidtype";
+function otherRecorderContainer(
+	selectedMedia) {
+
+	return selectedMedia === "vid" ?
+		"aud" : "vid";
 }
+
+
+const audioMediaConstraints = {
+	audio: true,
+	video: false,
+};
+
+
+const videoMediaConstraints = {
+
+
+	audio: true,
+	video: true,
+};
+
+function startRecording(
+	thisButton, otherButton) {
+
+	
+	navigator.mediaDevices.getUserMedia(
+		selectedMedia === "vid" ?
+		videoMediaConstraints :
+		audioMediaConstraints)
+		.then((mediaStream) => {
+
+		
+		const mediaRecorder =
+			new MediaRecorder(mediaStream);
+
+		
+		window.mediaStream = mediaStream;
+		
+		window.mediaRecorder = mediaRecorder;
+
+		mediaRecorder.start();
+
+		
+		mediaRecorder.ondataavailable = (e) => {
+
+		
+			chunks.push(e.data);
+		};
+
+	
+		mediaRecorder.onstop = () => {
+
+		
+			const blob = new Blob(
+				chunks, {
+					type: selectedMedia === "vid" ?
+						"video/mp4" : "audio/mpeg"
+				});
+			chunks = [];
+
+			
+			const recordedMedia = document.createElement(
+				selectedMedia === "vid" ? "video" : "audio");
+			recordedMedia.controls = true;
+
+			
+			const recordedMediaURL = URL.createObjectURL(blob);
+
+			
+			recordedMedia.src = recordedMediaURL;
+
+			
+			const downloadButton = document.createElement("a");
+
+			
+			downloadButton.download = "Recorded-Media";
+
+			downloadButton.href = recordedMediaURL;
+			downloadButton.innerText = "Send media";
+
+			downloadButton.onclick = () => {
+
+				
+				URL.revokeObjectURL(recordedMedia);
+			};
+
+			document.getElementById(
+				`${selectedMedia}-recorder`).append(
+				recordedMedia, downloadButton);
+		};
+
+		if (selectedMedia === "vid") {
+
+			
+			webCamContainer.srcObject = mediaStream;
+		}
+
+		document.getElementById(
+				`${selectedMedia}-record-status`)
+				.innerText = "Recording";
+
+		thisButton.disabled = true;
+		otherButton.disabled = false;
+	});
+}
+
+function stopRecording(thisButton, otherButton) {
+
+	
+	window.mediaRecorder.stop();
+
+	
+	window.mediaStream.getTracks()
+	.forEach((track) => {
+		track.stop();
+	});
+
+	document.getElementById(
+			`${selectedMedia}-record-status`)
+			.innerText = "Recording done!";
+	thisButton.disabled = true;
+	otherButton.disabled = false;
+}
+
 
 
 
