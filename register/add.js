d15fc8490a68adeabb3f594edc3d5d7d9ac8c07c
Zeenat <zeenatavontuur@gmail.com>
2022-08-07 05:36:15 +0200

    Zeenat

---
+++ register/add.js
@@ -0,0 +1,14 @@
+const number = document.querySelector(".NameEntered");
+const btn = document.querySelector(".button")
+
+btn.addEventListener("click", () => {
+    var storedNumber = list || []
+    
+        if ( storedNumber.push(personName)) {
+            return true
+        }
+
+        
+        
+
+})
